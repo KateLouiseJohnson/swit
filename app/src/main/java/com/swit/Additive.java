4102140a22193987f8d4e4ea8a2f7e071ed8b18d
KateLouiseJohnson <katelouisejohnson@gmail.com>
2016-05-20 00:12:22 +1000

    Added classes and search functionality

---
+++ app/src/main/java/com/swit/Additive.java
@@ -0,0 +1,21 @@
+package com.swit;
+
+/**
+ * Created by Miss Johnson on 19/05/2016.
+ */
+public class Additive {
+
+    public String code;
+    public String name;
+    public String desc;
+    public String warn;
+
+    public Additive(String code, String name, String desc, String warn){
+
+        this.code = code;
+        this.name = name;
+        this.desc = desc;
+        this.warn = warn;
+
+    }
+}
--- app/src/main/java/com/swit/Main.java
+++ app/src/main/java/com/swit/Main.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.swit.wword.WTree;
@@ -19,10 +20,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends AppCompatActivity {
 
     private Typeface testFont;
+    public Additives store = new Additives();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +51,9 @@ public class Main extends AppCompatActivity {
 
         WWord testFlakes = loadFromWord("flakes_small.jpg");
         searchFor(tree, testFlakes, "Flakes");
+
+        /*Kate's Sections --------------*/
+        loadContent();
     }
 
     public WWord loadFromWord(String filename) {
@@ -75,24 +82,44 @@ public class Main extends AppCompatActivity {
         }
     }
 
-    public String loadContents() {
+    /*
+    * Kate's Sections --------------
+    */
+
+    public String loadContent() {
         try {
             InputStream file = getAssets().open("SuperCoolMessage.txt");
-            BufferedReader r = new BufferedReader(new InputStreamReader(file));
-            StringBuilder total = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file));
             String line;
-            while ((line = r.readLine()) != null) {
-                total.append(line).append('\n');
+            List<String> list = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
             }
-            return total.toString();
+            this.store.LoadAdditives(list);
         } catch (IOException e) {
             Log.e("Error", "Unable to load contents!");
         }
         return null;
     }
 
-    public void makeItThing(View view) {
+    public String GetAdditive(String searchTerm){
+
+        if(store.list.containsKey(searchTerm)){
+            Additive additive = store.list.get(searchTerm);
+            String result = additive.code + "\n" +
+                    additive.name + "\n" +
+                    additive.desc + "\n" +
+                    additive.warn + "\n";
+            return result;
+        }
+
+        return "No Result Found -  You Lose!";
+    }
+
+    public void userSearch(View view) {
         TextView label = (TextView) findViewById(R.id.lbl_world);
-        label.setText(loadContents());
+        EditText userInput = (EditText) findViewById(R.id.editText);
+        String input = userInput.getText().toString();
+        label.setText(GetAdditive(input));
     }
 }
